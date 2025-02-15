b2dfab7782fdac71a1146c681cc454a15c56051a
Subhamay-Dey <ncdey1966@gmail.com>
2025-02-15 19:48:39 +0530

    Pricing section done ✅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Navbar from '@/components/home/Navbar'
 import { Footerdemo } from '@/components/home/Footer'
 import React from 'react'
-
+import PricingPage from '@/components/home/Pricing'
 function Page() {
   return (
     <>
       <Navbar/>
       <div className='block'>
+        <PricingPage/>
         <Footerdemo/>
       </div>
     </>
---
+++ src/components/home/Pricing.tsx
@@ -0,0 +1,53 @@
+import { PricingCards } from "../ui/pricing-cards";
+
+const tiers = [
+    {
+        name: "PRO",
+        price: 100,
+        description: "For small teams and growing businesses",
+        features: [
+            { name: "10 Podcast Summary", included: true },
+            { name: "Top Questions Highlight", included: true },
+            { name: "AI Powered Insights", included: true },
+            { name: "24/7 email support", included: true },
+            { name: "Enterprise features", included: false },
+            { name: "Get two Podcast Summary free 🚀", included: false},
+        ],
+        cta: {
+            text: "Get started",
+            href: "/signup",
+        }
+    },
+    {
+        name: "PREMIUM",
+        price: 500,
+        interval: "monthly",
+        description: "For large organizations and enterprises",
+        highlight: true,
+        features: [
+            { name: "52 Podcast Summary", included: true },
+            { name: "Top Questions Highlight", included: true },
+            { name: "AI Powered Insights", included: true },
+            { name: "24/7 priority support", included: true },
+            { name: "Enterprise features", included: true },
+            { name: "Get two Podcast Summary free 🚀", included: true},
+        ],
+        cta: {
+            text: "Contact sales",
+        }
+    },
+];
+
+function PricingPage() {
+    return (
+        <div className="container mx-auto px-4 py-12 md:px-6 lg:px-8">
+            <PricingCards 
+            tiers={tiers}
+            className="gap-6"
+            containerClassName="py-12"
+            />
+        </div>
+    );
+}
+
+export default PricingPage;
