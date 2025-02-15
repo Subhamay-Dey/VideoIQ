c1336ec482b74dce8ecc8ff014ab0f046e457522
Subhamay-Dey <ncdey1966@gmail.com>
2025-02-15 23:59:11 +0530

    Testimonials section done ✅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Navbar from '@/components/home/Navbar'
 import { Footerdemo } from '@/components/home/Footer'
 import React from 'react'
 import PricingPage from '@/components/home/Pricing'
+import { Testimonials } from '@/components/home/Testimonials'
 function Page() {
   return (
     <>
       <Navbar/>
       <div className='block'>
         <PricingPage/>
+        <Testimonials/>
         <Footerdemo/>
       </div>
     </>
