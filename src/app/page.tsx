dd40a695531e5ccb90cdebfaa891d69d845679dc
Subhamay-Dey <ncdey1966@gmail.com>
2025-02-16 17:24:24 +0530

    Add user session handling to Navbar component ✅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,16 @@ import React from 'react'
 import PricingPage from '@/components/home/Pricing'
 import { Testimonials } from '@/components/home/Testimonials'
 import { Hero } from '@/components/home/Hero'
+import { authOptions, CustomSession } from './api/auth/[...nextauth]/options'
+import { getServerSession } from 'next-auth'
 
-function Page() {
+async function Page() {
+
+  const session: CustomSession | null = await getServerSession(authOptions);
+
   return (
     <>
-      <Navbar/>
+      <Navbar user={session?.user}/>
       <Hero/>
       <div className='block'>
         <PricingPage/>
