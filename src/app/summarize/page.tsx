497b8f68effc45eddb3bee454fcf1bb27ab046d3
Subhamay-Dey <ncdey1966@gmail.com>
2025-02-23 23:57:47 +0530

    Add Summarize component to fetch and display summary based on ID ✅

---
+++ src/app/summarize/page.tsx
@@ -0,0 +1,22 @@
+import GetSummary from '@/actions/SummaryAction'
+import { notFound } from 'next/navigation'
+import React from 'react'
+
+async function Summarize({searchparams}: {searchparams: {[key: string]: string | undefined}}) {
+
+    if(!searchparams?.["id"]) {
+        return notFound()
+    }
+
+    const summary:SummaryType | null = await GetSummary.getSummary(searchparams?.["id"])
+
+    if(!summary) {
+        return notFound()
+    }
+
+  return (
+    <div>Summarize</div>
+  )
+}
+
+export default Summarize
