d0904d9e1ba9685834c6858176960b11d3aa1cbd
Subhamay-Dey <ncdey1966@gmail.com>
2025-02-27 02:11:40 +0530

    Implement Summarize service with user authentication and coin validation logic ✅

---
+++ src/app/api/services/Summarize.ts
@@ -0,0 +1,35 @@
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { authOptions, CustomSession } from "../auth/[...nextauth]/options";
+import getUserCoin from "@/actions/fetchActions";
+
+interface SummarizePayloadType {
+    url: string,
+    id: string,
+}
+
+class Summarize {
+    static async summarize(req:NextRequest) {
+        try {
+            const session:CustomSession | null = await getServerSession(authOptions);
+
+            if(!session) {
+                return NextResponse.json({message: "Unauthorized"}, {status: 401})
+            }
+            const body:SummarizePayloadType = await req.json()
+
+            const userCoins = await getUserCoin.getUserCoin(session.user?.id!);
+            const currentCoins = userCoins?.coins ?? 0;
+
+            if(userCoins == null || (userCoins?.coins < 10)) {
+                const coinsneeded = 10 - currentCoins;
+                return NextResponse.json({message: `You need ${coinsneeded} more coins to summarize. Please add more coins.`}, {status: 400});
+            }
+
+            
+
+        } catch (error) {
+            
+        }
+    }
+}
