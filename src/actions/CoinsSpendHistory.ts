9cee595068722c4903fc3156bc21b2f5a2d09270
Subhamay-Dey <ncdey1966@gmail.com>
2025-03-01 23:25:36 +0530

    Update middleware to match /summarize route, modify OldSummaryCard link for dynamic ID, and add CoinsSpendHistory action for fetching user coin spend history ✅

---
+++ src/actions/CoinsSpendHistory.ts
@@ -0,0 +1,25 @@
+import { revalidateTag, unstable_cache } from "next/cache";
+import prisma from "../../prisma/db.config";
+
+class CoinsSpendHistory {
+    static coinsspendhistory = unstable_cache(async(user_id: number | string) => {
+        return await prisma.coinSpend.findMany({
+            include: {
+                summary: {
+                    select: {
+                        id: true,
+                        url: true,
+                        title: true                    }
+                }
+            },
+            orderBy: {
+                id: "desc"
+            },
+            where: {
+                user_id: Number(user_id)
+            }
+        })
+    },
+    ["coinsSpend"],
+    {revalidate: 60 * 60, tags: ["coinsSpend"]})
+}
--- src/components/dashboard/OldSummary.tsx
+++ src/components/dashboard/OldSummary.tsx
@@ -10,7 +10,7 @@ export default function OldSummaryCard({
   return (
     <div className="bg-white shadow-md rounded-md p-4 h-52">
       <h1 className="text-lg font-semibold">{summary.title}</h1>
-      <Link href={`/summarize?id=${summary.id}`}>
+      <Link href={`/summarize/${summary.id}`}>
         <h1 className="">{summary.url}</h1>
       </Link>
       <p className="mt-2">
