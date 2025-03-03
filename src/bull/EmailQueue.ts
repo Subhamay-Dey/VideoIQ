67dd4ffead771f38182a55c767309d833c20464a
Subhamay-Dey <ncdey1966@gmail.com>
2025-03-04 02:58:19 +0530

    Added EmailQueue and EmailWorker for handling email sending and  passed the user's email, subject and text to whom the email will be send through nodemailer ✅

---
+++ src/bull/EmailQueue.ts
@@ -0,0 +1,33 @@
+import {Job, Queue, QueueEvents, Worker} from "bullmq"
+import { defaultQueueOptions, redisConnection } from "./queue";
+import { sendEmail } from "@/mails/mail";
+
+export const EmailQueueName:string = "emailqueue";
+
+export const EmailQueue:Queue = new Queue(EmailQueueName, {
+    connection: redisConnection,
+    defaultJobOptions: defaultQueueOptions,
+})
+
+export const EmailQueueEvents = new QueueEvents(EmailQueueName, {
+    connection: redisConnection,
+})
+
+export const EmailWorker = new Worker(EmailQueueName,
+    async(job:Job) => {
+
+        const {user} = job.data;
+
+        console.log(`Processing login for user: ${user.email}`);
+
+            await sendEmail(
+                user.email, 
+                "Welcome to VideoIQ 🎉", 
+                `Hi ${user.name}, You have successfully logged in, Best wishes from Subhamay 😊`
+            );
+
+            return { success: true, user };
+            
+    },
+    {connection:redisConnection},
+)
