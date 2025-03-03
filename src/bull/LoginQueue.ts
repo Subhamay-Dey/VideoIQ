957eb46852e6579a4ae389f4541f053e37d541d5
Subhamay-Dey <ncdey1966@gmail.com>
2025-03-03 23:56:00 +0530

    Implement user creation and retrieval in LoginWorker for enhanced login processing ✅

--- src/bull/LoginQueue.ts
+++ src/bull/LoginQueue.ts
@@ -16,6 +16,33 @@ export const loginQueueEvents = new QueueEvents(LoginQueueName, {
 export const LoginWorker = new Worker(LoginQueueName, 
     async(job:Job) => {
     console.log(job.data);
+    const {user, account} = job.data;
+    try {
+        const findUser = await prisma.user.findUnique({
+          where: {
+            email: user.email!,
+          },
+        });
+        if (findUser) {
+          user.id = findUser?.id.toString();
+          return true;
+        }
+
+        const data = await prisma.user.create({
+          data: {
+            email: user.email!,
+            name: user.name!,
+            oauth_id: account?.providerAccountId!,
+            provider: account?.provider!,
+            image: user?.image,
+          },
+        });
+
+        return { success: true, user: { id: data.id } };
+      } catch (error) {
+        console.error("Login processing failed:", error);
+      return { success: false, error };
+      }
     },
     {connection: redisConnection},
 )
