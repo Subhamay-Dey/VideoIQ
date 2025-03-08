6f2793651f7be891d9ae8b409a48992d18031e9b
Subhamay-Dey <ncdey1966@gmail.com>
2025-03-08 18:39:37 +0530

    Razorpay Service - Added Razorpay integration for transaction processing ✅

---
+++ src/app/api/services/Transactions.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import Razorpay from "razorpay";
+
+const razorpay = new Razorpay({
+    key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID!,
+    key_secret: process.env.RAZORPAY_SECRET_ID!
+});
+
+class Transaction {
+    static async transaction(req: NextRequest) {
+    const {amount,currency} = await req.json();
+
+    if (!amount || !currency) {
+        return NextResponse.json({ error: "Invalid payment details" }, { status: 400 });
+    }
+
+    const order  = await razorpay.orders.create({
+        amount,
+        currency
+    });
+
+    return NextResponse.json(order);
+    }
+}
+
+export {Transaction}
